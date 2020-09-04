75832bf23cbe98157b2fce6a7ef1e49c200da622
Виталик Матях <1seestars@MacBook-Air-Vitalik.local>
2020-09-04 20:09:03 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:1seestars/1seestars.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:1seestars/contact-list-app.git master:gh-pages
+# git push -f git@github.com:1seestars/contact-list-app.git master:gh-pages
 
 cd -
